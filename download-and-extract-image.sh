dd979fa7a29ac326db725b18dd5bbcc0bd7d836e
Reinhard <rkugler@sba-research.org>
2025-08-09 10:29:26 -0400

    simplified download-and-extract

--- download-and-extract-image.sh
+++ download-and-extract-image.sh
@@ -4,31 +4,19 @@ LOCAL_FILE="alpine-minirootfs-3.9.0-x86_64.tar.gz"
 TARGET_URL="http://dl-cdn.alpinelinux.org/alpine/v3.9/releases/x86_64/alpine-minirootfs-3.9.0-x86_64.tar.gz"
 CONTAINER_DIR="container"
 
-echo "downlading alpine"
-if [ ! -f $LOCAL_FILE ] ; then
-    curl -o "$LOCAL_FILE" "$TARGET_URL"
-    if [ $? -ne 0 ] ; then
-        echo "error downlading file" >&2
-	exit 1
-    fi
-fi
+perror()
+{
+    echo $* >&2
+    exit 1
+}
 
-echo "extracting to container"
-if [ -f $LOCAL_FILE ] ; then
-     if [ -d $CONTAINER_DIR ] ; then
-         echo "container directory '$CONTAINER_DIR' already present. Remove first: rm -rf $CONTAINER_DIR"
-	 exit 1
-     fi
-     mkdir $CONTAINER_DIR
-     if [ $? -ne 0 ] ; then
-         echo "error creating $CONTAINER_DIR"
-	 exit 1
-     fi
-     tar -xzpf $LOCAL_FILE -C $CONTAINER_DIR
-     if [ $? -ne 0 ] ; then
-         echo "warnings while extracting '$LOCAL_FILE'. mknod warnings are ok."
-     fi 
-     chmod 755 $CONTAINER_DIR
-fi
+[ ! -d $CONTAINER_DIR ] || perror "container directory '$CONTAINER_DIR' already present. Remove first: rm -rf $CONTAINER_DIR"
+# folder should not exist at this stage
+mkdir $CONTAINER_DIR || perror "error creating $CONTAINER_DIR"
+
+echo "downlading and extracting file"
+curl -o - "$TARGET_URL" | tar -xzp -C $CONTAINER_DIR || perror "error downlading file"
+
+chmod 755 $CONTAINER_DIR
 
 echo "Your container image can be found in '$CONTAINER_DIR'"
